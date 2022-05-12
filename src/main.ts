b8be1a990ebd852fd20a123806880a515fdb73c6
刘伟 <lemonpaimc@126.com>
2022-05-12 18:23:46 +0800

    feat(init): feat(init):

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import * as core from '@actions/core'
 import archiver from 'archiver'
+import path from 'path'
 import run from './run'
 
 async function main(): Promise<void> {
   const filepath = await run(
     core.getInput('type') as archiver.Format,
     core.getInput('name'),
-    core.getInput('input'),
-    core.getInput('output')
+    path.join(process.cwd(), core.getInput('input')),
+    path.join(process.cwd(), core.getInput('output'))
   )
   if (filepath) return core.setOutput('path', filepath)
   return core.setFailed(`${core.getInput('type')} error`)
