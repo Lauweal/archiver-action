25724a11c8391e938ee70522947a5947f75947c7
刘伟 <lemonpaimc@126.com>
2022-05-12 19:38:52 +0800

    feat:

--- dist/index.js
+++ dist/index.js
@@ -60,12 +60,31 @@ main();
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core = __importStar(__nccwpck_require__(2186));
 const archiver_1 = __importDefault(__nccwpck_require__(3084));
-const core_1 = __importDefault(__nccwpck_require__(2186));
 const fs_1 = __importDefault(__nccwpck_require__(5747));
 const path_1 = __importDefault(__nccwpck_require__(5622));
 function run(type, name, input, output) {
@@ -75,7 +94,7 @@ function run(type, name, input, output) {
         const stream = fs_1.default.createWriteStream(filepath);
         task.pipe(stream);
         task.on('close', () => {
-            core_1.default.info(`FILE ---> ${filepath}`);
+            core.info(`FILE ---> ${filepath}`);
             res(path_1.default.join(output, `${name}.${type}`));
         });
         task.on('error', () => {
--- src/run.ts
+++ src/run.ts
@@ -1,5 +1,5 @@
+import * as core from '@actions/core'
 import archiver from 'archiver'
-import core from '@actions/core'
 import fs from 'fs'
 import path from 'path'
 
